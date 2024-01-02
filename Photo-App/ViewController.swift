eae0556f431614c9cce4a644589d4ffad53e45f3
Sameer Limbu <sameerlimbu31@gmail.com>
2024-01-02 18:25:23 +0000

    Added the button UI

--- Photo-App/ViewController.swift
+++ Photo-App/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +31,28 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
-
         
+        view.addSubview(button)
+        getRandompic()
+    }
+    //Added the button
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(x: 30,
+                              y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-60,
+                              height: 55)
+    }
+    
+    //Get the random img
+    func getRandompic() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
 
 
